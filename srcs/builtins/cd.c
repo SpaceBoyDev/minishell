8369ae2caf74c3e5e8e082167c12a1984079eb2e
Darío <dario.mart.lop.03@gmail.com>
2025-06-10 21:52:10 +0200

    added: cd builtin

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,10 +6,37 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:27:08 by dario             #+#    #+#             */
-/*   Updated: 2025/06/10 20:40:35 by dario            ###   ########.fr       */
+/*   Updated: 2025/06/10 21:49:29 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+int	cd(char *path)
+{
+	char *home;
 
+	if (!path)
+	{
+		home = getenv("HOME");
+		if (!home)
+		{
+			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+			return (1);
+		}
+		if (chdir(home) != 0)
+		{
+			ft_putstr_fd("minishell: cd: HOME: No such file or directory", 2);
+			return (1);
+		}
+		return (0);
+	}
+	if (chdir(path) != 0)
+	{
+		ft_putstr_fd("minishell: cd: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": No such file or directory\n", 2);
+		return (1);
+	}
+	return (0);
+}
