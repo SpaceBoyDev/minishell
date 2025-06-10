c57397389ed4efacaace7a7aeddc6e64fe420e6c
Darío <dario.mart.lop.03@gmail.com>
2025-06-10 22:04:03 +0200

    fix: easier to read cd.c

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:27:08 by dario             #+#    #+#             */
-/*   Updated: 2025/06/10 21:49:29 by dario            ###   ########.fr       */
+/*   Updated: 2025/06/10 22:00:38 by dario            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,19 @@ int	cd(char *path)
 		home = getenv("HOME");
 		if (!home)
 		{
-			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
+			printf("minishell: cd: HOME not set\n");
 			return (1);
 		}
 		if (chdir(home) != 0)
 		{
-			ft_putstr_fd("minishell: cd: HOME: No such file or directory", 2);
+			printf("minishell: cd: HOME: No such file or directory");
 			return (1);
 		}
 		return (0);
 	}
 	if (chdir(path) != 0)
 	{
-		ft_putstr_fd("minishell: cd: ", 2);
-		ft_putstr_fd(path, 2);
-		ft_putstr_fd(": No such file or directory\n", 2);
+		printf("minishell: cd: %s: No such file or directory\n", path);
 		return (1);
 	}
 	return (0);
