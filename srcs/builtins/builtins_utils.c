f305721ad9aede7dcd5aeff8458822e9ee4af96f
marcos <marcoslopez1305@gmail.com>
2025-09-02 19:12:22 +0200

    fixed wrong comparison builtins_utils.c

--- srcs/builtins/builtins_utils.c
+++ srcs/builtins/builtins_utils.c
@@ -3,28 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   builtins_utils.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dario <dario@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marcos <marcos@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 02:28:27 by dario             #+#    #+#             */
-/*   Updated: 2025/07/28 21:01:16 by dario            ###   ########.fr       */
+/*   Updated: 2025/09/02 19:10:07 by marcos           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 #include "builtins.h"
 
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
 static bool	is_builtin(const char *str)
 {
 	size_t	len;
 
 	len = ft_strlen(str);
-	if (ft_strncmp(str, "echo", len) == 0
-		|| ft_strncmp(str, "cd", len) == 0
-		|| ft_strncmp(str, "pwd", len) == 0
-		|| ft_strncmp(str, "export", len) == 0
-		|| ft_strncmp(str, "unset", len) == 0
-		|| ft_strncmp(str, "env", len) == 0
-		|| ft_strncmp(str, "exit", len) == 0)
+	if (ft_strcmp(str, "echo") == 0
+		|| ft_strcmp(str, "cd") == 0
+		|| ft_strcmp(str, "pwd") == 0
+		|| ft_strcmp(str, "export") == 0
+		|| ft_strcmp(str, "unset") == 0
+		|| ft_strcmp(str, "env") == 0
+		|| ft_strcmp(str, "exit") == 0)
 		return (true);
 	return (false);
 }
@@ -33,17 +43,17 @@ int	exec_builtins(t_cmd *cmd, char **env)
 {
 	if (!is_builtin(cmd->cmd))
 		return (1);
-	if (ft_strncmp(cmd->cmd, "cd", ft_strlen(cmd->cmd)) == 0)
+	if (ft_strcmp(cmd->cmd, "cd") == 0)
 		return (ft_cd(cmd->args[1]));
-	else if (ft_strncmp(cmd->cmd, "echo", ft_strlen(cmd->cmd)) == 0)
+	else if (ft_strcmp(cmd->cmd, "echo") == 0)
 		return (ft_echo(cmd->args));
-	else if (ft_strncmp(cmd->cmd, "env", ft_strlen(cmd->cmd)) == 0)
+	else if (ft_strcmp(cmd->cmd, "env") == 0)
 		return (ft_env(env));
-	else if (ft_strncmp(cmd->cmd, "export", ft_strlen(cmd->cmd)) == 0)
+	else if (ft_strcmp(cmd->cmd, "export") == 0)
 		return (ft_export(cmd, env));
-	else if (ft_strncmp(cmd->cmd, "pwd", ft_strlen(cmd->cmd)) == 0)
+	else if (ft_strcmp(cmd->cmd, "pwd") == 0)
 		return (ft_pwd());
-	else if (ft_strncmp(cmd->cmd, "exit", ft_strlen(cmd->cmd)) == 0)
+	else if (ft_strcmp(cmd->cmd, "exit") == 0)
 	{
 		ft_exit();
 		return (0);
