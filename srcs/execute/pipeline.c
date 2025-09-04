351a46fefd2a97c218206bd3115700596dca6d8a
marcos <marcoslopez1305@gmail.com>
2025-09-04 18:48:16 +0200

    working on open() errors

--- srcs/execute/pipeline.c
+++ srcs/execute/pipeline.c
@@ -52,16 +52,24 @@ int	restore_io(t_cmd *cmd)
 	return (0);
 }
 
+// TODO: must check open() return value
 int	io_set(t_cmd *cmd)
 {
-	if (cmd-> infile && cmd->in_op == IN)
+	if (cmd->infile && cmd->in_op == IN)
 	{
+		close(0);
 		cmd->in_fd = open(cmd->infile, O_RDONLY);
+		if (cmd->in_fd == -1)
+		{
+			perror(cmd->infile);
+			return (0);
+		}
 		dup2(cmd->in_fd, 0);
 		close(cmd->in_fd);
 	}
 	else if (cmd->infile && cmd->in_op == HEREDOC)
 	{
+		dup2(cmd->in_std, 0);
 		cmd->in_fd = heredoc(cmd->infile);
 		dup2(cmd->in_fd, 0);
 		close(cmd->in_fd);
@@ -69,16 +77,26 @@ int	io_set(t_cmd *cmd)
 	if (cmd->outfile && cmd->out_op == OUT)
 	{
 		cmd->out_fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (cmd->in_fd == -1)
+		{
+			perror(cmd->outfile);
+			return (0);
+		}
 		dup2(cmd->out_fd, 1);
 		close(cmd->out_fd);
 	}
 	else if (cmd->outfile && cmd->out_op == APPEND)
 	{
 		cmd->out_fd = open(cmd->outfile, O_WRONLY | O_CREAT | O_APPEND, 0644);
+		if (cmd->out_fd == -1)
+		{
+			perror(cmd->outfile);
+			return (0);
+		}
 		dup2(cmd->out_fd, 1);
 		close(cmd->out_fd);
 	}
-	return (0);
+	return (1);
 }
 
 int	pipeline(t_cmd *cmd, char **env)
