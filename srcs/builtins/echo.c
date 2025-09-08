4429244b12a9f616fbfd9c207b6dfd83fca70f51
dario <dario.mart.lop.03@gmail.com>
2025-09-08 13:15:48 +0200

    echo little fix

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,32 +6,22 @@
 /*   By: darmarti <darmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:27:11 by dario             #+#    #+#             */
-/*   Updated: 2025/09/08 12:58:54 by darmarti         ###   ########.fr       */
+/*   Updated: 2025/09/08 13:14:58 by darmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 #include "../utils/utils.h"
-
-char	*create_env_var(char *var)
-{
-	char	*str;
-	int		i;
-
-	str = (char *)malloc(sizeof(char) * (ft_strlen(var)));
-	i = 0;
-	while (var[++i])
-		str[i - 1] = var[i];
-	str[i] = '\0';
-	return (str);
-}
+#include "../heredoc/heredoc.h"
 
 int	ft_echo(char **args, char **env)
 {
 	int		i;
 	bool	nl;
+	char	*env_var;
 
-	nl = (bool)ft_strncmp(args[1], "-n", ft_strlen(args[1]));
+	(void)env;
+	nl = (bool)ft_strcmp(args[1], "-n");
 	if (nl)
 		i = 1;
 	else
@@ -40,7 +30,9 @@ int	ft_echo(char **args, char **env)
 	{
 		if (args[i][0] == '$')
 		{
-			printf("%s", ft_getenv(create_env_var(args[i]), env));
+			env_var = expand(args[i], 0);
+			printf("%s", env_var);
+			free(env_var);
 		}
 		else
 			printf("%s", args[i]);
