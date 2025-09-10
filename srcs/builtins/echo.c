cee762c8657e19fe460557a700bc03803b4864de
dario <dario.mart.lop.03@gmail.com>
2025-09-10 18:39:18 +0200

    echo "fixed" removed var expansion

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcos <marcos@student.42.fr>              +#+  +:+       +#+        */
+/*   By: darmarti <darmarti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:27:11 by dario             #+#    #+#             */
-/*   Updated: 2025/09/09 16:42:40 by marcos           ###   ########.fr       */
+/*   Updated: 2025/09/10 18:37:37 by darmarti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,12 @@
 #include "../utils/utils.h"
 #include "../heredoc/heredoc.h"
 
-void	print_env_var(char *var, t_data *data)
-{
-	char	*env_var;
-
-	env_var = expand(var, data);
-	printf("%s", env_var);
-	free(env_var);
-}
-
 int	ft_echo(char **args, t_data *data)
 {
 	int		i;
 	bool	nl;
 
+	(void)data;
 	nl = (bool)ft_strcmp(args[1], "-n");
 	if (nl)
 		i = 1;
@@ -35,10 +27,7 @@ int	ft_echo(char **args, t_data *data)
 		i = 2;
 	while (args[i])
 	{
-		if (args[i][0] == '$')
-			print_env_var(args[i], data);
-		else
-			printf("%s", args[i]);
+		printf("%s", args[i]);
 		++i;
 		if (args[i])
 			printf(" ");
