1c61300149a7e140ef4f25bf136b2a92bea427b3
marcos <marcoslopez1305@gmail.com>
2025-09-10 19:00:20 +0200

    solved echo valgrind memory error

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: darmarti <darmarti@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marcolop <marcolop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 20:27:11 by dario             #+#    #+#             */
-/*   Updated: 2025/09/10 18:37:37 by darmarti         ###   ########.fr       */
+/*   Updated: 2025/09/10 18:59:03 by marcolop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@ int	ft_echo(char **args, t_data *data)
 	bool	nl;
 
 	(void)data;
+	if (!args[1])
+	{
+		printf("\n");
+		return (0);
+	}
 	nl = (bool)ft_strcmp(args[1], "-n");
 	if (nl)
 		i = 1;
